42194f511bd5df56c90c9b54aacfe8b6fde220bf
LH-and-FPGA <180024497+LH-and-FPGA@users.noreply.github.com>
2025-10-25 12:18:22 +0100

    Task2

---
+++ task2/sinegen.sv
@@ -0,0 +1,31 @@
+module sinegen #(
+    parameter COUNT_WIDTH = 8,
+               DATA_WIDTH  = 8
+) (
+    input logic [COUNT_WIDTH-1:0] incr,
+    input logic rst,
+    input logic en,
+    input logic clk,
+    output logic [DATA_WIDTH-1:0] dout1,
+    output logic [DATA_WIDTH-1:0] dout2
+);
+
+    logic [COUNT_WIDTH-1:0] addr;
+
+    counter Counter(
+        .rst(rst),
+        .en(en),
+        .incr(incr),
+        .clk(clk),
+        .count(addr)
+    );
+
+    rom Rom(
+        .clk(clk),
+        .addr1(addr),
+        .addr2(addr + incr),
+        .dout1(dout1),
+        .dout2(dout2)
+    );
+
+endmodule
---
+++ task2/sinegen_tb.cpp
@@ -0,0 +1,56 @@
+#include "Vsinegen.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // TODO: Check the doc about this
+
+    Vsinegen* sinegen = new Vsinegen;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    sinegen->trace(tfp, 99);
+    tfp->open("sinegen.vcd");
+
+    // init vbuddy
+    if (vbdOpen() != 1) return -1;
+    vbdHeader("Lab 2: Sinegen");
+    vbdSetMode(1);
+
+    sinegen->clk = 1;
+    sinegen->en = 1;
+    sinegen->rst = 0;
+    sinegen->incr = 1;
+
+    int half_cyc_counter = 0;
+
+
+    while(true) {
+        sinegen->incr = vbdValue();
+
+        vbdPlot(sinegen->dout1, -240, 360);
+        vbdPlot(sinegen->dout2, -240, 360);
+
+        for (clk = 0; clk < 2; clk++) {
+            // tfp->dump(2*i+clk);
+            sinegen->clk = !sinegen->clk;
+            sinegen->eval();
+        }
+
+        if (vbdGetkey() == 'q') {
+            vbdClose();
+            tfp->close();
+            exit(0);
+        }
+        // i++;
+    }
+
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
